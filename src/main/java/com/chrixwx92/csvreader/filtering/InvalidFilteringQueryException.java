3d542c5d57cb7281c98b223a1468fe66b2338ed7
ChrixWX92 <chris@wards-net.co.uk>
2023-07-19 17:25:28 +0100

    Update InvalidFilteringQueryException.java

--- src/main/java/com/chrixwx92/csvreader/filtering/InvalidFilteringQueryException.java
+++ src/main/java/com/chrixwx92/csvreader/filtering/InvalidFilteringQueryException.java
@@ -2,6 +2,6 @@ package com.chrixwx92.csvreader.filtering;
 
 public class InvalidFilteringQueryException extends IllegalArgumentException {
     public InvalidFilteringQueryException() {
-        super("Invalid arguments passed to selected filtering option.");
+        super("Invalid argument(s) passed to selected filtering option.");
     }
 }
